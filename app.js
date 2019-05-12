520102d29e75361a4e28b8b66f2377828a052b5f
Earth <pimanus1234@gmail.com>
2019-05-12 14:57:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require("fs"); //อ่านไฟล์ user.json
 
+app.get('/getuser', function (req, res){
+
+    fs.readFile(__dirname + "/" + "user.json", "utf-8", function( err, data){
+    console.log ( data ); // ข้อมูลของ user ใน user.json ทุกคน
+    res.end( data ); 
+    });
+});
 
 var server = app.listen(8081, function (){
     var host = server.address().address
